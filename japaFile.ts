95d9b96a2762ead131f06880a0ae1461dc548e27
yson <magoyeison@gmail.com>
2020-08-22 19:05:12 -0400

    config http

--- japaFile.ts
+++ japaFile.ts
@@ -1,5 +1,9 @@
 import 'reflect-metadata'
-import { join } from 'path'
+import execa from 'execa'
+
+// Import helpers from path module
+import { join, isAbsolute, sep } from 'path'
+
 import getPort from 'get-port'
 import { configure } from 'japa'
 import sourceMapSupport from 'source-map-support'
@@ -8,6 +12,27 @@ process.env.NODE_ENV = 'testing'
 process.env.ADONIS_ACE_CWD = join(__dirname, '..')
 sourceMapSupport.install({ handleUncaughtExceptions: false })
 
+/**
+ * [runMigrations description]
+ */
+async function runMigrations () {
+  await execa.node('ace', ['migration:run'], {
+    stdio: 'inherit',
+  })
+}
+
+/**
+ * [rollbackMigrations description]
+ */
+async function rollbackMigrations () {
+  await execa.node('ace', ['migration:rollback'], {
+    stdio: 'inherit',
+  })
+}
+
+/**
+ * [startHttpServer description]
+ */
 async function startHttpServer () {
   const { Ignitor } = await import('@adonisjs/core/build/src/Ignitor')
   process.env.PORT = String(await getPort())
@@ -15,13 +40,33 @@ async function startHttpServer () {
 }
 
 /**
+ * [getTestFiles description]
+ */
+function getTestFiles () {
+  let userDefined = process.argv.slice(2)[0]
+  if (!userDefined) {
+    return 'build/test/**/*.spec.js'
+  }
+
+  if (isAbsolute(userDefined)) {
+    userDefined = userDefined.endsWith('.ts')
+      ? userDefined.replace(`${join(__dirname, '..')}${sep}`, '')
+      : userDefined.replace(`${join(__dirname)}${sep}`, '')
+  }
+
+  return `build/${userDefined.replace(/\.ts$|\.js$/, '')}.js`
+}
+
+/**
  * Configure test runner
  */
 configure({
-  files: [
-    'build/test/**/*.spec.js',
-  ],
+  files: getTestFiles(),
   before: [
+    runMigrations,
     startHttpServer,
   ],
+  after: [
+    rollbackMigrations,
+  ]
 })
