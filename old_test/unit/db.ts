8e3bcfec9045b3f4976e4086fe2fda462eb081fb
magocod <magoyeison@gmail.com>
2022-02-27 21:39:15 -0400

    failed load chance

--- old_test/unit/db.ts
+++ old_test/unit/db.ts
@@ -1,9 +1,9 @@
-import { assert } from 'chai'
+import test from 'japa'
 
 import User from 'App/Models/User'
 
-describe('sample', function () {
-  it('ensure user password gets hashed during save', async function () {
+test.group('User password', () => {
+  test('ensure user password gets hashed during save', async (assert) => {
     const user = await User.create({
       first_name: 'admin',
       last_name: 'admin',
---
+++ test/sample.spec.ts
@@ -0,0 +1,7 @@
+import { assert } from 'chai'
+
+describe('sample', function () {
+  it('assert sum', async function () {
+    assert.equal(2 + 2, 4)
+  })
+})
